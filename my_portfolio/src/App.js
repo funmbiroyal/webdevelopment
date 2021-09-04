ce999f2763c0cded4b58e326fd5a18debf01e415
Grace <funmibiajadi@gmail.com>
2021-09-04 23:28:24 +0100

    Modified App Component

---
+++ my_portfolio/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './reusables/Header';
+import Grace from './Grace';
+import Footer from './reusables/Footer';
+
+
+function App() {
+  return (
+   <div className="App">
+   
+ <Header title="funmbiRoyal"/>
+ <Grace /> 
+ <Footer />
+    
+    
+   </div>
+  );
+}
+
+export default App;
