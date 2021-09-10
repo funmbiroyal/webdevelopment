2eefda8d0acde63ddb8f18e34fec44a5c3058fdb
Grace <funmibiajadi@gmail.com>
2021-09-10 16:19:26 +0100

    RoomContainer component

---
+++ my_login_page/src/house/RoomContainer.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import Room from './Room'
+
+const RoomContainer = ({pillar}) =>{
+    const [rooms,setRooms] = useState([])
+
+    const showRooms = () => {
+        let roomAmount = Math.floor(pillar/4)
+        let newArray = new Array(roomAmount)
+        newArray.fill(roomAmount)
+        setRooms(newArray)
+    }
+
+    useEffect(()=>{
+        showRooms()
+    },[pillar])
+
+    return(
+        <div>
+            {rooms.map((room,index) => <Room key={index} /> )}
+        </div>
+    )
+}
+
+export default RoomContainer;
