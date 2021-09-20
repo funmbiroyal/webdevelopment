f9903601f933f9991559593ebbce6b3475ecf31b
Grace <funmibiajadi@gmail.com>
2021-09-20 23:11:26 +0100

    About component, my_portfolio

---
+++ my_portfolio/src/reusables/About.js
@@ -0,0 +1,40 @@
+const About = ()=>{
+    return(
+        <div className = "allAbout">
+            <div className ="aboutSection">
+            <h1>About</h1>
+            <p>I specialize in rapidly building software companies and web applications.<br/> I use an entrepreneurial 
+            mindset to inform my design and engineering output</p>
+
+           <p>I prioritize communication while building. I am an excellent communicator, <br/>
+               both written and verbal. I use screen casts, video calls,<br/> and written words to
+                lead teams built on trust and transparency.</p>
+
+           <p>I am an expert in JavaScript, working primarily in React and Firebase, <br/>
+               but I have experience working in a wide range of technologies including Node
+                & <br/>Express, MongoDB, PostgreSQL, React Native,Gatsby,REST API design, CircleCI, <br/>
+                3rd party APIs (Stripe, Discord, Slack, Amplitude, Google Auth, etc.), 
+                AWS (S3, CloudFront,<br/> Cognito, Build Pipeline,Route53, ACM), and many more.</p>
+
+           <p>Additionally, I am an experienced interface and user experience designer. I use Figma
+                to<br/> communicate ideas through low and high fidelity mockups, design systems,
+                 and clickable<br/> prototypes.</p>
+                 </div>
+                 <div className="personalSection">
+                 <h1>Personal</h1>
+                <p> I’ve been a gamer my entire life. I play StarCraft,
+                <br/> Fortnite, and Nintendo 64.</p>
+
+                 <p>I ran Division 3 track (400m) at Rochester Institute of Technology,<br/> and still 
+                     hold school records to this day.</p>
+
+                 <p> I play guitar and bass, and I’m learning to record my own songs.</p>
+
+                  I collect video games and action figures, and love SciFi.
+                 </div>
+        </div>
+    )
+}
+
+
+export default About
