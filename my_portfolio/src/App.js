a2cb9350e9914329cb3c79e2c203fef76db6f6cd
Grace <funmibiajadi@gmail.com>
2021-09-20 23:15:53 +0100

    App component,my_portfolio

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -3,15 +3,39 @@ import './App.css';
 import Header from './reusables/Header';
 import Grace from './Grace';
 import Footer from './reusables/Footer';
+import ArticlePage from './reusables/ArticlePage';
+
+import {useState} from 'react'
+import About from './reusables/About';
 
 
 function App() {
+  const [page,setPage] = useState('home')
+
+  const navs = [
+   {
+     title:'home',
+     component: Grace
+   },
+   {
+     title:'articles',
+     component: ArticlePage
+   },
+   {
+    title:'about',
+    component:About
+   }
+  ]
+
   return (
    <div className="App">
    
- <Header title="funmbiRoyal"/>
- <Grace /> 
- <Footer />
+  <Header title="funmbiRoyal" setPage = {setPage}/>
+    {
+      navs.map(nav=>nav.title === page ? <nav.component/>:"")
+    }
+ <Footer /> 
+ 
     
     
    </div>
