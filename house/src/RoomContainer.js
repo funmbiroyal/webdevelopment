f30872880a710702759c847062d1b70b29168652
Grace <funmibiajadi@gmail.com>
2021-09-26 22:07:47 +0100

    Room Container component

---
+++ house/src/RoomContainer.js
@@ -0,0 +1,28 @@
+import React,{useState,useEffect} from 'react'
+import Room from './components/Room'
+ import { useSelector } from 'react-redux';
+
+const RoomContainer = () => {
+    const pillarData = useSelector(state => state.pillarState)
+    const {pillar} = pillarData
+
+    const [rooms, setRooms] = useState([])
+    const showRooms = ()=>{
+        let roomAmount = Math.floor(pillar / 4);
+        let newArray = new Array (roomAmount);
+        newArray.fill(roomAmount);
+        setRooms(newArray);
+    };
+
+    useEffect(() => {
+        showRooms()
+    }, [pillar]);
+
+    return (
+        <div>
+            {rooms.map((room,index)=>(<Room key = {index} />))}
+        </div>
+    )
+}
+
+export default RoomContainer
