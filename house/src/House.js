afd32d5f1cbc27091aaf831950bf10ca565003fc
Grace <funmibiajadi@gmail.com>
2021-09-26 22:06:58 +0100

    House Component

---
+++ house/src/House.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Foundation from './Foundation'
+import RoomContainer from './RoomContainer'
+import Roof from './components/Roof'
+
+const House = ({pillar}) => {
+    return (
+        <div className ='house'>
+           {pillar >= 4 && <Roof/>}
+           {pillar ? ((pillar >= 4 || pillar % 4 === 0) && <RoomContainer pillar = {pillar} />) : 'No Room'}
+           <Foundation />
+        </div>
+    )
+}
+
+export default House
